b20068bf60c228cb2bed80eb5d7fe6aa11a13589
Gonzaga <153249635+pegonser@users.noreply.github.com>
2025-02-21 07:29:47 -0300

    Added one \ for every \
    Windows...

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ let url = "not-placeholder";
 
 while (url != placeholder) {
     url = prompt("paste URL");
-    alert(`cd "C:\Users\nicea\3F\YouTube" && yt-dlp -f "bestvideo+bestaudio" --merge-output-format mp4 "${url}"`);
+    alert(`cd "C:\\Users\\nicea\\3F\\YouTube" && yt-dlp -f "bestvideo+bestaudio" --merge-output-format mp4 "${url}"`);
 }
